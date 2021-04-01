c8fbae47e39e0a379c8a13f9fdd5c9d8b316526e
Metarya Ruparel <ruparel.metarya1988@gmail.com>
2021-03-31 21:47:08 -0400

    cleaning data

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -9,7 +9,18 @@ class AnomalyDetection:
 
         :return:
         """
-        raw_data = pd.read_csv('value.txt')
+        raw_data = pd.read_csv("/Users/Metarya/Downloads/trace_201708/container_usage.csv", header=None,
+                               names=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'])
+        del raw_data["2"]
+        del raw_data["4"]
+        del raw_data["5"]
+        del raw_data["6"]
+        del raw_data["7"]
+        del raw_data["8"]
+        del raw_data["9"]
+        del raw_data["10"]
+        del raw_data["11"]
+        del raw_data["12"]
         print(raw_data)
 
     @staticmethod
