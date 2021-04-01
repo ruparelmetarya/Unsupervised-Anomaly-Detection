16f961fb9b38e697e9cf4094e6a212516b367ce2
Sriram Ramesh <grksriram321@gmail.com>
2021-04-01 01:02:42 -0400

    Add graphs for auto regression

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+from math import sqrt
+from sklearn.metrics import mean_squared_error
 import pylab
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
@@ -7,10 +10,29 @@ from random import random
 from datetime import datetime
 # import the necessary packages
 from matplotlib import pyplot as plt
-def dateparse (time_in_secs):
-    return datetime.fromtimestamp(float(time_in_secs))
+
+# create a difference transform of the dataset
+def difference(dataset):
+    diff = list()
+    for i in range(1, len(dataset)):
+        value = dataset[i] - dataset[i - 1]
+        diff.append(value)
+    return np.array(diff)
+
+def getValue(diff,first):
+    res = list()
+    diff[0] = diff[0] + first
+    for i in range(1, len(diff)-1):
+        diff[i] = diff[i] +  diff[i-1]
+    return np.array(diff)
 
 
+# Make a prediction give regression coefficients and lag obs
+def predict(coef, history):
+    yhat = coef[0]
+    for i in range(1, len(coef)):
+        yhat += coef[i] * history[-i]
+    return yhat
 # out = x.truncate(before=datetime.datetime(2015,12,2,12,2,18))
 class AnomalyDetection:
 
@@ -20,7 +42,10 @@ class AnomalyDetection:
 
         :return:
         """
-        data = pd.read_csv('ec2_cpu_utilization_24ae8d.csv',parse_dates=['timestamp'],index_col=['timestamp'])
+        data = pd.read_csv('ec2_cpu_utilization_24ae8d.csv',
+                           parse_dates=['timestamp'],
+                           index_col=['timestamp'],
+                           squeeze=True)
         # plt.rcParams['figure.figsize']=(20,10)
         # plt.style.use('ggplot')
         # data.plot()
@@ -34,23 +59,48 @@ class AnomalyDetection:
         :return:
         """
 
+    # @staticmethod
+    # def ar2(series):
+    #     X = difference(series.values)
+    #     size = int(len(X) * 0.66)
+    #     train, test = X[0:size], X[size:]
+    #     t2 = series.values[size:]
+    #     # train autoregression
+    #     window = 6
+    #     model = AutoReg(train, lags=6)
+    #     model_fit = model.fit()
+    #     coef = model_fit.params
+    #     # walk forward over time steps in test
+    #     history = [train[i] for i in range(len(train))]
+    #     predictions = list()
+    #     for t in range(len(test)):
+    #         yhat = predict(coef, history)
+    #         obs = test[t]
+    #         predictions.append(yhat)
+    #         history.append(obs)
+    #     rmse = sqrt(mean_squared_error(test, predictions))
+    #     print('Test RMSE: %.3f' % rmse)
+    #     # plot
+    #     # plt.plot(test)
+    #     # test2 = getValue(test)
+    #     plt.plot(t2)
+    #     print(series.values[size-3:size+3])
+    #     plt.plot(predictions, color='red')
+    #     plt.show()
+
     @staticmethod
     def auto_regression(dataframe):
         """
 
         :return:
         """
-        # fit model
-        # X = dataframe.dropna().squeeze()
-        train = dataframe[1:len(dataframe)-20]
+        train = dataframe[1:len(dataframe)-200]
         print("len(dataframe):"+str(len(dataframe)))
-        # test= train
-        test = dataframe.tail(20)
+        test = dataframe.tail(200)
 
 
 
-        # train, test = train_test_split(dataframe, test_size=0.20, shuffle=False)
-        model = AutoReg(train.squeeze(), lags=[1, 11, 12])
+        model = AutoReg(train, lags=[1, 11, 12])
         model_fit = model.fit()
         print(model_fit.summary())
         # make predictions
@@ -63,19 +113,9 @@ class AnomalyDetection:
         # create a comparison dataframe
         compare_df = pd.concat([test,
                                 predictions], axis =1).rename(
-                 columns={'stationary': 'actual', 0:'predicted'})
-        # pylab.show()
-        #plot the two values
+                                    columns={'stationary': 'actual', 0:'predicted'})
         plt.plot(compare_df)
         plt.show()
-        # compare_df.plot()
-
-
-        # r2 = r2_score(sales_data['stationary'].tail(12), predictions)
-
-        # make prediction
-        # yhat = model_fit.predict(len(data), len(data))
-        # print(yhat)
 
 
 if __name__ == '__main__':
