d93f9dc609f22b7c3bce531e6faa23ca452d0020
Metarya Ruparel <ruparel.metarya1988@gmail.com>
2021-03-31 21:24:35 -0400

    Anomaly Detection Skeletion.

---
+++ anomaly_detection.py
@@ -0,0 +1,17 @@
+class AnomalyDetection:
+
+    def moving_average(self):
+        """
+
+        :return:
+        """
+
+    def auto_regression(self):
+        """
+
+        :return:
+        """
+
+
+if __name__ == '__main__':
+    print("Hi")
